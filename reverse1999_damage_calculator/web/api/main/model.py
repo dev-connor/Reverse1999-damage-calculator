9ab755c59160d4511dd90ee542fd801b892cb774
Connor <kor.connor@gmail.com>
2023-12-28 18:18:57 +0900

    데미지 계산기 추가

--- reverse1999_damage_calculator/web/api/main/model.py
+++ reverse1999_damage_calculator/web/api/main/model.py
@@ -15,3 +15,22 @@ class DEF:
     buff: int = Form(0)
     portray: int = Form(0)
 
+@dataclass
+class DMG:
+    # 아군
+    atk: int = Form(0)
+    power: int = Form(0)
+    stat_bonus: int = Form(0)
+    inherit_atk: int = Form(0)
+    inherit_bonus: int = Form(0)
+    spell: int = Form(0)
+
+    # 적군
+    defence: int = Form(0)
+    dmg_taken: int = Form(0)
+    afflatus: bool = Form(False) # 영감
+
+    # 환경
+    def_redn: int = Form(0)
+    buff: int = Form(0)
+    debuff: int = Form(0)
